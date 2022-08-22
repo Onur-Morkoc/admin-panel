2d58d7182397defadcc40352e86398250e112b03
Onur-Morkoc <onurmorkoc00@gmail.com>
2022-08-23 02:20:38 +0300

    Added react-router-dom to redirect for home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/home/Home';
 
 function App() {
   return (
-    <div className="App">Admin Panel</div>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+
+          <Route index path='/' element={<Home />} />
+
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
